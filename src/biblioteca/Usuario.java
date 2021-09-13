316ba257725aca235121037231baecdccc6d4a8a
Eduardo <eduardo.sballico@gmail.com>
2021-09-13 14:07:34 -0300

    Multas e histórico de usuário

--- src/biblioteca/Usuario.java
+++ src/biblioteca/Usuario.java
@@ -4,12 +4,17 @@ public class Usuario {
 	
 	private String nome;
 	private int id;
+	private String historico;
 	
 	public void cadastrar() {
 		
 	}
 	
 	public void excluir() {
+		
+	}
+	
+	public void multar() {
 		
 	}
 
